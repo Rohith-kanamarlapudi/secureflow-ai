a23513ab79c65c8e5cca127765b3143f91484fe3
Rohith k <tejasrirohith@gmail.com>
2026-08-14 23:50:18 +0530

    feat: update API client authentication

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,40 +1,48 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: "http://localhost:8000",
 });
 
+// ============================================================
+// Request interceptor
+// ============================================================
+
 api.interceptors.request.use(
-  (config) => {
-    const token =
-      localStorage.getItem("access_token");
+    (config) => {
+        const token = localStorage.getItem("access_token");
 
-    if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+        if (token) {
+            config.headers = config.headers || {};
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        /*
+         * IMPORTANT:
+         *
+         * When sending FormData, do NOT manually set
+         * Content-Type.
+         *
+         * The browser/Axios will automatically generate:
+         *
+         * multipart/form-data; boundary=...
+         *
+         * FastAPI needs that boundary to correctly detect
+         * the uploaded "file" field.
+         */
+
+        if (config.data instanceof FormData) {
+            if (config.headers) {
+                delete config.headers["Content-Type"];
+                delete config.headers["content-type"];
+            }
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    console.error(
-      "API ERROR:",
-      error.response?.status,
-      error.response?.data
-    );
-
-    return Promise.reject(error);
-  }
-);
+export default api;
