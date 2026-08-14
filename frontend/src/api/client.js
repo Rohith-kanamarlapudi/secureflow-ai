96c713f4ea98ceb293efa0233e124975936d6039
Rohith k <tejasrirohith@gmail.com>
2026-08-14 23:32:51 +0530

    feat(frontend): add authenticated API client for backend communication

---
+++ frontend/src/api/client.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://localhost:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token =
+      localStorage.getItem("access_token");
+
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error(
+      "API ERROR:",
+      error.response?.status,
+      error.response?.data
+    );
+
+    return Promise.reject(error);
+  }
+);
